ad0ba2f813a706a15b1d65d4815bbec7e7983cf3
ahmed-shabbir74 <474786474a@gmail.com>
2023-03-08 00:33:00 +0500

    add router and controllers

---
+++ controllers/fun.js
@@ -0,0 +1,31 @@
+exports.home =function (req,res){
+
+    res.send("home page");   
+};
+
+exports.plot =function (req,res){
+
+    res.send("plot page");   
+};
+
+exports.house =function (req,res){
+
+    res.send("house page");   
+};
+
+exports.rent =function (req,res){
+
+    res.send("rent page");   
+};
+exports.about =function (req,res){
+
+    res.send("about page");   
+};
+
+
+exports.contactus =function (req,res){
+
+    res.send("contact page");   
+};
+
+
