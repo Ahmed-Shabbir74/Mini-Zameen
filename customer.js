0cc0c70e5724ae6f859a1ac6251b23e6858b0add
alibhatti00786 <125507855+alibhatti00786@users.noreply.github.com>
2023-04-18 13:24:37 +0500

    routes

--- customer.js
+++ customer.js
@@ -1,106 +1,14 @@
 //jshint esversion:6
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = require("../app");
+const router = require('express').Router();
+const customer = require('../controllers/customer.controller');
 
-let customerSchema = sequelize.define('customer', {
-    customerid:{
-        type:Number,
-        allowNull: false,
-        validate: {
-            notNull: {
-                msg: 'Enter ID'
-            }
-        }
-    },
-    customerpassword:{
-        type:String,
-        allowNull: false,
-        validate: {
-            notNull: {
-                msg: 'Password is required'
-            }
-        }
-    },
-    firstname:{
-        type:String,
-        allowNull: false,
-        validate: {
-            notNull: {
-                msg: 'First Name is required'
-            }
-        }
-    },
-    middlename:{
-        type:String
-    },
-    lastname:{
-        type:String,
-        allowNull: false,
-        validate: {
-            notNull: {
-                msg: 'Last name is required'
-            }
-        }
-    },
-    emailid:{
-        type:String,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: {
-                msg: 'Invalid e-mail.'
-            }
-        }
-    },
-    dateofbirth:{
-        type:Date,
-        allowNull: false,
-        validate: {
-            notNull: {
-                msg: 'Birthdate is required'
-            }
-        }
-    },
-    phonenumber:{
-        type:String,
-        allowNull: false,
-        validate: {
-            notNull: {
-                msg: 'Phone number is required'
-            },
-            len: {
-                args: [10, 10],
-                msg: 'Phone number must be exactly 10 digits'
-            }
-        }
-    },
-    occupation:{
-        type:String,
-        allowNull: false,
-        validate: {
-            notNull: {
-                msg: 'Occupation is required'
-            }
-        }
-    },
-    annualincome:{
-        type:Number,
-        allowNull: false,
-        validate: {
-            notNull: {
-                msg: 'Annual Income is required'
-            }
-        }
-    },
-    address:{
-        type:String,
-        allowNull: false,
-        validate: {
-            notNull: {
-                msg: 'Address is required'
-            }
-        }
-    }
-});
 
-module.exports = customerSchema;
+router.get("/login", customer.loginGet);
+
+router.post("/register", customer.register);
+
+router.post("/regsub", customer.regsub);
+
+router.post("/login", customer.login);
+
+module.exports = router;
